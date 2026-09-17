3b333cc1ab4ceeff940f50c5030280af6f59d79d
НиКо <niki.machtec@gmail.com>
2026-09-17 13:30:03 +0300

    Change Firebase API key
    Updated Firebase API key in firebase.js.

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyZJkgwGCN3AE7kbZuU_VklAvskT1R4Wk",
+  apiKey: "AIzaSyBcCcijox6SNH4Zzeh16jlnzeqTzONMVhs",
   authDomain: "caritas-vitania.firebaseapp.com",
   databaseURL: "https://caritas-vitania-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "caritas-vitania",
