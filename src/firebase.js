abddbcb1697c36a168481d67194020eeebcb5fea
НиКо <niki.machtec@gmail.com>
2026-09-17 08:29:59 +0300

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyZJkgwGCN3AE7kbZuU_VKlAvskT1R4Wk",
+  apiKey: "AIzaSyAyZJkgwGCN3AE7kbZuU_VklAvskT1R4Wk",
   authDomain: "caritas-vitania.firebaseapp.com",
   databaseURL: "https://caritas-vitania-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "caritas-vitania",
@@ -17,6 +13,6 @@ const firebaseConfig = {
   measurementId: "G-VPJFE2EXWD"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
